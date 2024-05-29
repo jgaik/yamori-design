0bb828f88441e7695466c88b9c7cca52ea33a15d
Jakub Gaik <jgaik@microstrategy.com>
2024-05-30 00:16:41 +0200

    add styles base

--- packages/components/src/components/button/button.tsx
+++
@@ -1,7 +0,0 @@
-import React, { ComponentPropsWithoutRef } from 'react';
-
-export type ButtonProps = ComponentPropsWithoutRef<'button'>;
-
-export const Button: React.FC<ButtonProps> = (props) => {
-  return <button {...props} />;
-};
---
+++ packages/components/src/utils/classnames.ts
@@ -0,0 +1,6 @@
+/**
+ * Create a class name with the given name.
+ */
+export function createComponentClassName(name: string) {
+  return `yamori-${name}`;
+}

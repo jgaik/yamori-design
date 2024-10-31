5ff9d7edc1a8921a4ef97fed196664dc271ebae1
jgaik <jakub.gaik@gmail.com>
2024-10-31 16:25:14 +0100

    prettier

--- apps/docs/eslint.config.mjs
+++ apps/docs/eslint.config.mjs
@@ -3,13 +3,15 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  {
-    extends: [pluginStorybook.configs['flat/recommended'], pluginJs.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
+export default tseslint.config({
+  extends: [
+    pluginStorybook.configs["flat/recommended"],
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+  ],
+  files: ["**/*.{ts,tsx}"],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser,
   },
-)
+});
